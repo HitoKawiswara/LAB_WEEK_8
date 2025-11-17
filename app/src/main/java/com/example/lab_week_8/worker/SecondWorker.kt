d9e8953b48b4c97bdee83740ab02cb7bcec9a48d
HitoKawiswara <hitokawiswara@gmail.com>
2025-11-17 16:03:48 +0700

    Commit No. 1 – add first & second worker

---
+++ app/src/main/java/com/example/lab_week_8/worker/SecondWorker.kt
@@ -0,0 +1,26 @@
+package com.example.lab_week_08.worker
+
+import android.content.Context
+import androidx.work.Data
+import androidx.work.ListenableWorker.Result
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class SecondWorker(
+    context: Context,
+    workerParams: WorkerParameters
+) : Worker(context, workerParams) {
+
+    override fun doWork(): Result {
+        val id = inputData.getString(INPUT_DATA_ID) ?: "unknown"
+        val output = Data.Builder()
+            .putString(OUTPUT_DATA_ID, "second-$id")
+            .build()
+        return Result.success(output)
+    }
+
+    companion object {
+        const val INPUT_DATA_ID = "inId"
+        const val OUTPUT_DATA_ID = "outId2"
+    }
+}
